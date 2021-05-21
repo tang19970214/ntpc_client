0ba226430ce7f30ef3ec8127900829843ec2aeeb
coderken <jk0910948659@gmail.com>
2021-05-21 10:12:56 +0800

    Add transition

--- src/views/Ｑuestion/question.vue
+++ src/views/Ｑuestion/question.vue
@@ -15,10 +15,13 @@
         <div class="question__list__quest" @click="isRotate = !isRotate">
           <i class="el-icon-plus mr-8" :class="{ rotate: isRotate }"></i
           >在交通平台系統最早可以預約訂車期間為何?
-          <p></p>
         </div>
-        <div class="question__list__ans " v-if="isRotate">
-          最早預約訂車期間為30天，最晚於72小時前至系統完成預約下單。
+        <div class="question__list__ans">
+          <transition name="fade">
+            <p v-if="isRotate">
+              最早預約訂車期間為30天，最晚於72小時前至系統完成預約下單。
+            </p>
+          </transition>
         </div>
       </div>
     </div>
@@ -87,6 +90,14 @@ export default {
         color: #888;
       }
     }
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
   }
 }
 </style>
